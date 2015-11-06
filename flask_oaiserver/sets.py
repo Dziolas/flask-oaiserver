f97e6801429a4823663ac1b24ae1db5dda0bd0ba
Wojciech Ziółek <wojciech.ziolek@cern.ch>
2015-11-06 18:07:59 +0100

    sets: New file for set helper functions
    Signed-off-by: Wojciech Ziółek <wojciech.ziolek@cern.ch>

---
+++ flask_oaiserver/sets.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Flask-OAIServer
+# Copyright (C) 2015 CERN.
+#
+# Flask-OAIServer is free software; you can redistribute it and/or
+# modify it under the terms of the Revised BSD License; see LICENSE
+# file for more details.
+
+"""Sets helper functions."""
+
+
+def get_sets_list():
+    return []
